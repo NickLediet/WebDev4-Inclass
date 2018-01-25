481ac1cd11c4947ac725afbbcddd71ef15189f43
Nick Lediet <nicklediet@gmail.com>
2018-01-25 09:33:53 -0500

    Initial Commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <title>Welcome to the Finest Selection of Blu-rays on the internets!</title>
+  <?php @include "./includes/styles.html" ?>
+</head>
+<body>
+
+  <div class="container">
+    <div class="col-8">
+      <h1>Hello World</h1>
+    </div>
+  </div>
+
+  <?php @include "./includes/scripts.html" ?>
+</body>
+</html>
